9ce3d8307a6cb92c0823b574f958aaa1950b5860
hchong16 <root@Shuku.localdomain>
2019-10-21 21:40:35 -0400

    Finished HW3

--- HW3/hw3server.js
+++ HW3/hw3server.js
@@ -1,4 +1,3 @@
-// Author: Harry Chong
 var express = require('express');
 var request =require('request');
 var app = express();
@@ -8,17 +7,21 @@ app.use(express.static("."));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+/* Get Credientials */
 var credentials = require('./aeriskey.json');
 var CLIENT_SECRET = credentials.secret;
 var CLIENT_ID = credentials.id
 
+/* Get Weather Forecast through API call, parse out JSON response and return to client.
+   Retrieve Timestamp, Weather Description, Minimum Temp (F), Maximum Temp (F), Icon Name, 
+   FeelsLike (F), Precepitation, and Humidity */
 app.get('/getweather', function (req, res) {
   var URL = "https://api.aerisapi.com/forecasts/" + req.query.lat + "," + req.query.lon + "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET;
   console.log(URL);
   request.get(URL, function(error, response, body){
     //console.log(body);
     var json = JSON.parse(body);
-	var validTime = [];
+	var timeStamp = [];
 	var weather = [];
 	var minTempF = [];
 	var maxTempF = [];
@@ -30,7 +33,17 @@ app.get('/getweather', function (req, res) {
 	// Parse Data into arrays:
 	// Extract Date/Time
 	for (var i = 0; i < 7; i++) {
-		validTime = validTime.concat(json.response[0].periods[i].validTime);
+		
+		// Time in Epoch format: Convert to readable format
+		var temp = json.response[0].periods[i].timestamp;
+		var date = new Date(temp * 1000);
+
+		var year = date.getFullYear();
+		var month = date.getMonth() + 1;
+		var day = date.getDate();
+		
+		var formatted = month + "-" + day + "-" + year;
+		timeStamp = timeStamp.concat(formatted);
 	};
 
 	// Extract Weather Description
@@ -69,10 +82,28 @@ app.get('/getweather', function (req, res) {
 	}
 	
 	// Combine into list of arrays
-	var data = [validTime, weather, minTempF, maxTempF, icon, feelsLikeF, pop, humidity]
+	var data = [timeStamp, weather, minTempF, maxTempF, icon, feelsLikeF, pop, humidity]
 	res.write(JSON.stringify(data)); 
     res.end();
   }); 
+});
+
+/* Get Location Name and State from API call based on GeoLocation and parse out JSON response
+   and send to Client. */
+app.get('/getlocation', function (req, res) {
+  var URL = "https://api.aerisapi.com/places/closest?p=" + req.query.lat + "," + req.query.lon + "&format=json&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET;
+  console.log(URL);
+  request.get(URL, function(error, response, body){
+    console.log(body);
+    var json = JSON.parse(body);
+	
+	var place = json.response[0].place.name;
+	var state = json.response[0].place.state;
+	var data = [place, state]
+
+    res.write(JSON.stringify(data));
+    res.end();
+  });
 });
 
 app.listen(8080, function(){
--- HW3/weather.js
+++ HW3/weather.js
@@ -1,6 +1,97 @@
-// Reset Table by removing existing rows
+// Function to reset table by removing existing rows
 function resetTable() {
-	$(".table tbody tr").remove();
+    $(".table tbody tr").remove();
 }
 
+// Function to get Geographic Location, Location Name/State, and Forecast
+function getInfo() {
+    // Obtain geographic coordinates.
+    navigator.geolocation.getCurrentPosition(success, error);
+	 
+    // Request Weather and Location based on Lat. and Lon. if above function passes.
+    function success(pos) {
+        var lat = pos.coords.latitude;
+        var lon = pos.coords.longitude;
+        document.getElementById('geolocation').innerHTML = "Lat: " + lat + " Lon: " + lon;
+		
+        requestWeather(lat, lon);
+        requestLocation(lat, lon);
+    }
 
+    // Function to return Weather Forecast.
+    function requestWeather(lat, lon) {
+        var URL = "./getweather";
+        $.ajax({
+            type: "GET",
+            url: URL,
+            dataType: "text",
+            timeout: 3000,
+            data : {"lat": lat,
+                    "lon": lon},
+            success : function(msg){
+                var data = JSON.parse(msg)
+		  
+		        var time_arr = data[0]
+		        var weather_des_arr = data[1]
+		        var minTemp_arr = data[2]
+		        var maxTemp_arr = data[3]
+		        var icon_arr = data[4]
+		        var feelsLikeF_arr = data[5]
+		        var pop = data[6]
+		        var humidity = data[7]
+			
+		        var tbody = $('#body');
+		        var count = 0;
+		        var totalCells = 1;
+		  
+                // Fill Table
+		        for (var i = 0; i < 7; i++) {
+                    var tr = $('<tr/>').appendTo(tbody);
+                    tr.append('<td class="table-light">' + time_arr[count] + '</td' + '</td>');
+                    tr.append('<td class="table-light">' + maxTemp_arr[count] + "° / " + minTemp_arr[count] + '°</td>');
+			        tr.append('<td class="table-light">' + feelsLikeF_arr[count] + '°</td>');
+			        tr.append('<td class="table-light">' + pop[count] + '%</td>');
+			        tr.append('<td class="table-light">' + humidity[count] + '%</td>');
+			        tr.append('<td class="table-light">' + weather_des_arr[count] + '</td>');
+			        tr.append('<td class="table-light"><img src="/css/icons/' +  icon_arr[count] + '"alt="" hspace="50" height=50 width=50></img></td>');
+                    count++;
+                } 
+				
+				// Remove error message if it existed
+				document.getElementById('current').innerHTML = "";
+            },
+            error: function(jgXHR, textStatus, errorThrown) {
+                alert("Error: " + textStatus + " " + errorThrown);
+                error() 
+		    }
+	    });
+    }
+  
+    // Function to return Location name/state.
+    function requestLocation(lat, lon) {
+	    var URL = "./getlocation";
+	    $.ajax({
+            type: "GET",
+            url: URL,
+            dataType: "text",
+            timeout: 3000,
+            data : {"lat": lat,
+                    "lon": lon},
+            success : function(msg){
+                var data = JSON.parse(msg)
+		        var place = data[0];
+		        var state = data[1];
+                document.getElementById('location').innerHTML = place + "," + state;
+		    },
+		    error: function(jgXHR, textStatus, errorThrown) {
+			    alert("Error: " + textStatus + " " + errorThrown);
+			    error() 
+		    }
+	    });
+    }
+  
+    // If Browser cannot obtain geographic coordinates, return error message.
+    function error() {
+        document.getElementById('current').innerHTML = "There was an error! Please try again later.";
+    }
+}
